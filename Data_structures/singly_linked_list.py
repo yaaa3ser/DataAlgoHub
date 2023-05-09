18ad3dbe28ee83046e133226289a139ca242ff14
yaaa3ser <yasserissa1411@gmail.com>
2023-05-09 18:01:01 +0300

    finish linkedlist implementation and its readme

---
+++ Data_structures/singly_linked_list.py
@@ -0,0 +1,216 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+    
+    def retrieve(self):
+        current = self.head
+        while current != None:
+            print(current.data, end=" ")
+            current = current.next
+        print()
+    
+    def size(self):
+        return self.size
+    
+    def is_empty(self):
+        return self.size == 0 # or self.head == None
+    
+    # returns the value of the nth item (starting at 0 for first)
+    def value_at(self, index):
+        if index >= self.size or index < 0:
+            return None
+        else:
+            current = self.head
+            for i in range(index):
+                current = current.next
+            return current.data
+        
+    # adds an item to the front of the list
+    def push_front(self, value):
+        new_node = Node(value)
+        new_node.next = self.head
+        self.head = new_node
+        self.size += 1
+    
+    # remove front item and return its value
+    def pop_front(self):
+        if self.is_empty():
+            return None
+        else:
+            current = self.head
+            self.head = current.next
+            self.size -= 1
+            return current.data
+        
+    # adds an item at the end
+    def push_back(self, value):
+        new_node = Node(value)
+        if self.is_empty():
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+        self.tail = new_node
+        self.size += 1
+    
+    # removes end item and returns its value
+    def pop_back(self):
+        if self.is_empty():
+            return None
+        if self.size == 1:
+            self.tail = None
+            return self.pop_front()
+        else:
+            current = self.head
+            while current.next != self.tail:
+                current = current.next
+            current.next = None
+            self.tail = current
+            self.size -= 1
+            return current.data
+    
+    # get value of front item
+    def front(self):
+        return self.head.data
+    
+    # get value of end item
+    def back(self):
+        return self.tail.data
+    
+    # insert value at index, so current item at that index is pointed to by new item at index
+    def insert(self, index, value):
+        if index > self.size or index < 0:
+            return None
+        elif index == 0:
+            self.push_front(value)
+        elif index == self.size:
+            self.push_back(value)
+        else:
+            current = self.head
+            for i in range(index-1):
+                current = current.next
+            # cnt = 0
+            # while cnt < index-1:
+            #     current = current.next
+            #     cnt+=1
+            new_node = Node(value)
+            new_node.next = current.next
+            current.next = new_node
+            self.size += 1
+    
+    # removes node at given index
+    def erase(self, index):
+        if index >= self.size or index < 0:
+            return None
+        elif index == 0:
+            self.pop_front()
+        elif index == self.size-1:
+            self.pop_back()
+        else:
+            current = self.head
+            for i in range(index-1):
+                current = current.next
+            current.next = current.next.next
+            self.size -= 1
+    
+    # removes the first item in the list with this value
+    def remove_value(self, value):
+        current = self.head
+        if current.data == value:
+            self.pop_front()
+            if self.size == 0:
+                self.tail = None
+            return
+        while current.next != None:
+            if current.next.data == value:
+                current.next = current.next.next
+                self.size -= 1
+                return
+            current = current.next
+        return None
+    
+    # reverses the list
+    def reverse(self):
+        if self.size == 0:
+            return
+        current = self.head
+        prev = None
+        while current != None:
+            next = current.next
+            current.next = prev
+            prev = current
+            current = next
+        self.head = prev
+    
+    # returns the value of the node at nth position from the end of the list
+    def value_n_from_end(self, n):
+        if n >= self.size or n < 0:
+            return None
+        else:
+            current = self.head
+            for i in range(self.size-n-1):
+                current = current.next
+            return current.data
+    
+# "============================================="
+# "================= TESTING ==================="
+# "============================================="        
+ll = LinkedList()
+# print(ll.is_empty())
+# print(ll.size)
+# print(ll.value_at(0))
+# ll.push_front(1)
+# print(ll.is_empty())
+# print(ll.size)
+# print(ll.value_at(0))
+# ll.retrieve()
+# ll.push_front(2)
+# ll.push_front(3)
+# ll.retrieve()
+# print(ll.value_at(0))
+# print(ll.value_at(1))
+# print(ll.value_at(2))
+# print(ll.value_at(3))
+# ll.retrieve()
+# print(ll.pop_front())
+# ll.retrieve()
+# print(ll.pop_front())
+# ll.retrieve()
+# print(ll.pop_front())
+# ll.retrieve()
+
+# ll.push_back(1)
+# ll.push_back(2)
+# ll.push_back(5)
+# ll.retrieve()
+# ll.pop_back()
+# ll.retrieve()
+# ll.pop_front()
+# ll.retrieve()
+# print(ll.size)
+# print(ll.is_empty())
+# ll.pop_back()
+# print(ll.is_empty())
+
+
+# ll.push_back(1)
+# ll.push_back(2)
+# ll.push_back(3)
+# ll.insert(1, 5)
+# ll.retrieve()
+# ll.reverse()
+# ll.retrieve()
+# print(ll.value_n_from_end(1))
+# ll.remove_value(2)
+# ll.remove_value(1)
+# ll.remove_value(3)
+# ll.retrieve()
+# print(ll.size)
+# print(ll.head, ll.tail)
--- README.md
+++ README.md
@@ -6,7 +6,12 @@ personal reference repository for common algorithms and data structures implemen
 - [Arrays](#arrays)
     - [Static Array](#static-array)
     - [Dynamic Array](#dynamic-array)
-    - [Summary](#summary)
+    - [Summary](#summary_0)
+- [Linked List](#linked-list)
+    - [Singly Linked List](#singly-linked-list)
+    - [Doubly Linked List](#doubly-linked-list)
+    - [Circular Linked List](#circular-linked-list)
+    - [Summary](#summary_1)
 
 <br/>
 <br/>
@@ -51,8 +56,7 @@ personal reference repository for common algorithms and data structures implemen
 <br />
 
 
-<div style="text-align:center"
->ــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ</div>
+
 
 # Arrays
 
@@ -77,10 +81,88 @@ personal reference repository for common algorithms and data structures implemen
  - **Idea**: store pointer to a dynamically allocated array and replace it with a newly-allocated array as needed.
  - [Dynamic Array Implementation](./Data_structures/array.py)
 
-### Summary
+### Summary_0
 
 - Unlike static arrays, dynamic arrays can be resized.
 - Appending an item to a dynamic array is O(1) on average, but O(n) worst-case, because of the possibility of having to allocate a new array and copy over the old elements.
 - Some space is wasted the capacity is always at least the length of the array, but usually, it's somewhere between length and length * 2.
-<div style="text-align:center"
->ــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ</div>
+
+
+
+# Linked List
+  - is a linear data structure, in which the elements are not stored at contiguous memory locations. The elements in a linked list are linked using pointers.
+  - data structure for storing objects in a linear fashion. It is a collection of nodes where each node holds a node value and a link to the next node in the list.
+  <br/><br/>
+  - **Advantages**:
+    - Dynamic size
+    - Ease of insertion/deletion
+  - **Disadvantages**:
+    - Random access is not allowed. We have to access elements sequentially starting from the first node. So we cannot do binary search with linked lists efficiently with its default implementation.
+    - Extra memory space for a pointer is required with each element of the list.
+    - Not cache friendly. Since array elements are contiguous locations, there is locality of reference which is not there in case of linked lists.
+  - **Use Cases**:
+    - Linked lists are used to implement stacks, queues, graphs, etc.
+    - dynamic memory allocation
+<table>
+  <thead>
+    <tr>
+      <th></th>
+      <th>Array</th>
+      <th>Linked List</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th>Order determined by</th>
+      <td>indices</td>
+      <td>pointers</td>
+    </tr>
+    <tr>
+      <th>Search</th>
+      <td>O(1)</td>
+      <td>O(n)</td>
+    </tr>
+  </tbody>
+</table>
+
+    
+  - The order of the elements in an array is determined by their indices. For example, if we have an array of integers {1, 2, 3, 4}, then the first element is at index 0, the second element is at index 1, and so on.
+
+  - The order of the elements in a linked list is determined by the pointers that connect the nodes together. For example, if we have a linked list with three nodes containing the data values 1, 2, and 3, then the order of the elements is determined by the pointers connecting the nodes: node 1 points to node 2, which points to node 3.
+
+
+## Types of Linked List
+
+### Singly Linked List
+
+- *Singly Linked Lists*: is a data structure that contains a sequence of nodes such that each node contains an object and a reference to the next node in the list.
+
+  - *Singly Linked Lists*
+
+    <img src="https://www.geeksforgeeks.org/wp-content/uploads/gq/2013/03/Linkedlist_insert_at_start.png" style = "width:80%" alt = "Figure for Singly Linked Lists">
+
+- #### Singly Linked List Implementation
+
+  - GOTO [Singly Linked Lists Implementation](./Data_structures/singly_linked_list.py)
+
+### Doubly Linked List
+
+- *Doubly Linked Lists*: is a data structure that contains a sequence of nodes such that each node contains an object and references to the previous and next nodes in the list.
+
+  - *Doubly Linked List*
+
+    <img src="https://www.geeksforgeeks.org/wp-content/uploads/gq/2014/03/DLL1.png" style = "width:80%" alt = "Figure for Doubly Linked Lists">
+
+### Circular Linked List
+- *Circular Linked Lists*: is a variation of a linked list in which the last element is linked to the first element. This forms a circular loop.
+
+  - *Circular Linked List*
+
+    <img src="https://media.geeksforgeeks.org/wp-content/uploads/CircularLinkeList.png" style = "width:80%" alt = "Figure for Circular Linked Lists">
+
+### Summary_1
+  - Unlike arrays, linked list elements are not stored at contiguous locations; the elements are linked using pointers.
+  - Accessing an element in a linked list is a linear operation because we have to sequentially traverse the list until we find the desired element.
+  - Linked lists have dynamic size, unlike arrays which have fixed size. So, it is not required to pre-allocate memory for a linked list.
+  - The drawbacks of using linked list are that they require extra memory for pointers and they are not cache friendly (elements are not stored in contiguous locations).
+  
